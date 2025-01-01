f3f6cedeaa6ca947802de6a4198acb96fe001510
Frank <L0N@users.noreply.github.com>
2025-01-01 21:22:36 +1000

    Integrated Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./_site/**/*.html",
+    "./*.html",
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/**/*.html"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
